27274647f26eb72745199df2181196defae8f4bf
Kira <shkiden@mail.ru>
2025-09-10 15:46:16 +0300

    feat: Add factorial.pas file

---
+++ factorial.pas
@@ -0,0 +1,20 @@
+﻿program FactorialCalculator;
+
+var
+  n, i: integer;     
+  factorial: int64; 
+
+begin
+  write('Введите натуральное число: ');
+  readln(n); 
+
+    for i := 1 to n do
+    begin
+      factorial := factorial * i;
+    end;
+
+    writeln('Факториал числа ', n, ' равен ', factorial);
+  end;
+
+  readln;
+end.
